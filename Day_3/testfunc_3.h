877cbeb63f1e2f2a3d7ec0e6bf7bac6be046f557
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-07-19 11:25:59 +0900

    Add files via upload

---
+++ Day_3/testfunc_3.h
@@ -0,0 +1,66 @@
+#ifndef _TEST_FUNC
+#define _TEST_FUNC
+
+
+#define PIN_SET_ON  GPIO_PIN_SET
+#define PIN_SET_OFF GPIO_PIN_RESET
+
+#define BTN_STATE_ON  GPIO_PIN_RESET
+#define BTN_STATE_OFF GPIO_PIN_SET
+
+#define TURN_ON_BTN    10
+#define TURN_OFF_BTN   11
+#define CHANGE_STATE_BTN   12
+
+extern uint8_t TestSetMode;
+
+#define TESTSET_BTN_INTER       100  /*must set btn pin GPIO_INTERRUPT and regenerate code!!"*/
+#define TESTSET_BTN_READ        101  /*must set btn pin GPIO_IN andd regenerate code!!"*/
+
+extern uint8_t TestAppMode;
+
+extern uint8_t bStartBlink;
+
+#define TESTAPP_NONE            199    
+#define TESTAPP_ONOFF_ALL       200 
+#define TESTAPP_STEP_BY_STEP    201  
+#define TESTAPP_TOGGLE_ALL      202
+#define TESTAPP_SHIFT           203
+#define TESTAPP_SHIFT_ROUND     204
+
+#define LED_CNT 8
+
+
+void SetLedOn(uint32_t LED_Pin, uint8_t bOn );
+
+uint8_t ReadButton(uint32_t Btn_Pin);
+
+void setBlinkStart(uint8_t bStart);
+
+void BlinkTest(void);
+
+void setAllLed(uint8_t bOn);
+
+void setAllToggleLed(uint8_t bOn);
+
+void doToggleTest(void);
+
+void setBtnTest_AllLedOn(void);
+
+void setBlinkTest(void);
+
+void doBlinkTest(void);
+
+void setLedShiftTest(uint8_t bOn);
+
+void doLedShiftTest(void);
+
+void doLedShiftRoundTest(void);
+
+void doBtnProc(void);
+
+void doTestProc(void);
+
+void printbin(uint8_t input);
+
+#endif //_TEST_FUNC
