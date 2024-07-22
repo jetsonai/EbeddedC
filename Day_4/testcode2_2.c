f2898cc4b9a0d3214115bcb9571714249221cb1a
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-07-22 14:34:00 +0900

    Update testcode2_2.c

--- Day_4/testcode2_2.c
+++ Day_4/testcode2_2.c
@@ -143,10 +143,24 @@ void fooFuncTest(void)
 //------------------------------
 
 
+
 int32_t fibNI(int32_t n) 
 {
-
+   int32_t previous = -1;
+   int32_t result = 1;
+   int32_t i=0;
+   int32_t sum=0;
+   for (i = 0; i <= n; i++) 
+   {
+      sum = result + previous;
+      previous = result;
+      result = sum;
+      //printf("i:%d result:%d\n", i, result); 
+   } 
+   return result;
 }
+
+
 
 int32_t fibN(int32_t n) {
 
